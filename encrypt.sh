9204d84d63791c8e9513d0bca988e9925c6dd357
ComasSky <hadrien@Bureau>
2025-03-27 21:29:02 +0100

    Gitignore + --yes option

--- encrypt.sh
+++ encrypt.sh
@@ -2,7 +2,7 @@
 clear
 read -p "Parent seed : " seed
 read -s -p "Password : " password
-echo "$seed" | gpg --symmetric --batch --passphrase "$password" --cipher-algo AES256 --output /tmp/seed.data
+echo "$seed" | gpg --symmetric --batch --yes --passphrase "$password" --cipher-algo AES256 --output /tmp/seed.data
 #Clear and remove CLI history
 clear && printf '\e[3J'
 echo "File seed.data created"
