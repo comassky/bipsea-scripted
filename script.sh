0e84377cd6288395faf8780bf75186fe813a5f5c
ComasSky <48486646+comassky@users.noreply.github.com>
2025-03-27 20:26:24 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 clear
 read -s -p "Password: " password
 printf "\n"
-DECODED=`gpg --decrypt --no-symkey-cache --passphrase $password --batch seed.data 2>/dev/null`
+DECODED=`gpg --decrypt --no-symkey-cache --passphrase "$password" --batch seed.data 2>/dev/null`
 cmd='bipsea validate -m "$DECODED" | bipsea xprv |  bipsea derive -a mnemonic -n "$1" -i "$2"'
 printf "QRCode of $1 words, index $2 \n"
 derivated=$(eval "$cmd")
